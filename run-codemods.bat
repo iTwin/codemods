90c8c8e483de428f32a09c7a9d148b8fc098e64a
Seamus Kirby <32379572+skirby1996@users.noreply.github.com>
2021-11-30 15:26:03 -0500

    Add script to update package.json, handle js and jsx package renames

--- run-codemods.bat
+++ run-codemods.bat
@@ -1,10 +1,11 @@
 @echo off
-if NOT "%2" == "" if "%3" == "" goto START
-echo Error: Incorrect arguments provided, please specify path to tsconfig and src directory with
-echo run-codemods.bat TSCONFIG_PATH SRC_PATH
+if NOT "%3" == "" if "%4" == "" goto START
+echo Error: Incorrect arguments provided, please specify path to package.json, tsconfig, and src directory with
+echo run-codemods.bat PACKAGE.JSON_PATH TSCONFIG_PATH SRC_PATH
 goto :EOF
 
 :START
-call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\typed-transforms.ts --extensions=ts,tsx --tsConfigPath=%1 %2
-call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=ts --parser=ts %2
-call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=tsx --parser=tsx %2
+@REM call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\typed-transforms.ts --extensions=ts,tsx --tsConfigPath=%2 %3
+call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=js,ts --parser=ts %3
+call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=jsx,tsx --parser=tsx %3
+call node %~dp0transforms\update-packagejson.js %1
