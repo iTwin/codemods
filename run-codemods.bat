c39920071152eef4d0558063fd294a025287ca4d
Seamus Kirby <32379572+skirby1996@users.noreply.github.com>
2021-11-30 15:26:28 -0500

    fix commented part of script

--- run-codemods.bat
+++ run-codemods.bat
@@ -5,7 +5,7 @@ echo run-codemods.bat PACKAGE.JSON_PATH TSCONFIG_PATH SRC_PATH
 goto :EOF
 
 :START
-@REM call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\typed-transforms.ts --extensions=ts,tsx --tsConfigPath=%2 %3
+call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\typed-transforms.ts --extensions=ts,tsx --tsConfigPath=%2 %3
 call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=js,ts --parser=ts %3
 call %~dp0node_modules\.bin\jscodeshift -t %~dp0transforms\itwin-codemods.ts --extensions=jsx,tsx --parser=tsx %3
 call node %~dp0transforms\update-packagejson.js %1
