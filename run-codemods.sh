d06688758d1a30c508a6c9c0f480285909ac36c4
Michael Belousov <mike.belousov@bentley.com>
2022-01-06 10:43:25 -0500

    support non-zsh (but require realpath which is a coreutil)

--- run-codemods.sh
+++ run-codemods.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/sh
 
 # Designed for use on Macs. May work on other *nix OSes.
 
@@ -7,7 +7,7 @@ if [ "$#" -ne 3 ]; then
   echo "    run-codemods.sh PACKAGE.JSON_PATH TSCONFIG_PATH SRC_PATH"
   exit 1
 fi
-root="${0:A:h}"
+root="$(dirname $(realpath $0))"
 "${root}/node_modules/.bin/jscodeshift" -t "${root}/transforms/typed-transforms.ts" --extensions=ts,tsx "--tsConfigPath=$2" "$3"
 "${root}/node_modules/.bin/jscodeshift" -t "${root}/transforms/itwin-codemods.ts" --extensions=js,ts --parser=ts "$3"
 "${root}/node_modules/.bin/jscodeshift" -t "${root}/transforms/itwin-codemods.ts" --extensions=jsx,tsx --parser=tsx "$3"
